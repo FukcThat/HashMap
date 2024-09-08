7eeafbf7ebdd13f67526953faf62f96975b9ef63
FukcThat <63652e636f6465@gmail.com>
2024-09-08 19:30:31 +0200

    Init commit

---
+++ script.js
@@ -0,0 +1,13 @@
+// Create hashmap Class
+// Create hash(key) function (has function with key as input)
+// Create set(key, value) function avoid collisions using %16
+// Create get(key) that returns the value assigned to that key, otherwise null
+// Create has(key) returns true if key is in hashmap. otherwise false
+// remove(key), if key exists in hashmap, remove entry and return true, if key doesnt exist return false
+// length() returns number of stored keys
+// clear() removes all entries in hashmap
+// keys() returns array containing all keys
+// values() returns array containing all the values
+// entries() returns array that contains each key-value pair
+
+console.log(5);
